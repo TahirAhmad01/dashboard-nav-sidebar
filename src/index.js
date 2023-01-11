d89e77229d39f5491abf7b90d15e6e12c783e12e
TahirAhmad01 <tahirahmad0234@gmail.com>
2023-01-11 20:22:04 +0600

    minor change

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./assets/css/main.css";
-import "flowbite/dist/flowbite.min.js";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
